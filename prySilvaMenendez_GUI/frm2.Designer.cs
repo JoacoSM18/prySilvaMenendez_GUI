ed7de47ccf410d40e75a1f2d27f6627127d7a441
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-12 12:29:12 -0300

    Nombre Controles y Segundo frm

---
+++ prySilvaMenendez_GUI/frm2.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace prySilvaMenendez_GUI
+{
+    partial class frm2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre1 = new System.Windows.Forms.Label();
+            this.lblNombre2 = new System.Windows.Forms.Label();
+            this.lblNombre3 = new System.Windows.Forms.Label();
+            this.txtNombre1 = new System.Windows.Forms.TextBox();
+            this.txtNombre2 = new System.Windows.Forms.TextBox();
+            this.txtNombre3 = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre1
+            // 
+            this.lblNombre1.AutoSize = true;
+            this.lblNombre1.Location = new System.Drawing.Point(108, 86);
+            this.lblNombre1.Name = "lblNombre1";
+            this.lblNombre1.Size = new System.Drawing.Size(94, 13);
+            this.lblNombre1.TabIndex = 0;
+            this.lblNombre1.Text = "Ingresar Nombre 1";
+            // 
+            // lblNombre2
+            // 
+            this.lblNombre2.AutoSize = true;
+            this.lblNombre2.Location = new System.Drawing.Point(108, 159);
+            this.lblNombre2.Name = "lblNombre2";
+            this.lblNombre2.Size = new System.Drawing.Size(94, 13);
+            this.lblNombre2.TabIndex = 1;
+            this.lblNombre2.Text = "Ingresar Nombre 2";
+            // 
+            // lblNombre3
+            // 
+            this.lblNombre3.AutoSize = true;
+            this.lblNombre3.Location = new System.Drawing.Point(109, 238);
+            this.lblNombre3.Name = "lblNombre3";
+            this.lblNombre3.Size = new System.Drawing.Size(94, 13);
+            this.lblNombre3.TabIndex = 2;
+            this.lblNombre3.Text = "Ingresar Nombre 3";
+            // 
+            // txtNombre1
+            // 
+            this.txtNombre1.Location = new System.Drawing.Point(259, 79);
+            this.txtNombre1.Name = "txtNombre1";
+            this.txtNombre1.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre1.TabIndex = 3;
+            // 
+            // txtNombre2
+            // 
+            this.txtNombre2.Location = new System.Drawing.Point(259, 152);
+            this.txtNombre2.Name = "txtNombre2";
+            this.txtNombre2.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre2.TabIndex = 4;
+            // 
+            // txtNombre3
+            // 
+            this.txtNombre3.Location = new System.Drawing.Point(259, 238);
+            this.txtNombre3.Name = "txtNombre3";
+            this.txtNombre3.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre3.TabIndex = 5;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(112, 341);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(69, 32);
+            this.btnCancelar.TabIndex = 6;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(331, 341);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(69, 32);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // frm2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txtNombre3);
+            this.Controls.Add(this.txtNombre2);
+            this.Controls.Add(this.txtNombre1);
+            this.Controls.Add(this.lblNombre3);
+            this.Controls.Add(this.lblNombre2);
+            this.Controls.Add(this.lblNombre1);
+            this.Name = "frm2";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre1;
+        private System.Windows.Forms.Label lblNombre2;
+        private System.Windows.Forms.Label lblNombre3;
+        private System.Windows.Forms.TextBox txtNombre1;
+        private System.Windows.Forms.TextBox txtNombre2;
+        private System.Windows.Forms.TextBox txtNombre3;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
