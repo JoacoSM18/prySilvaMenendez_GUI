e40dd0a6c55ace0a88b52154bab7253352dc8b91
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-17 13:03:58 -0300

    Iconos en frmInicio y en Aplicacion

--- prySilvaMenendez_GUI/frmControles.Designer.cs
+++ prySilvaMenendez_GUI/frmControles.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmControles));
             this.btnAbrir = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -37,10 +38,9 @@
             this.btnAbrir.BackColor = System.Drawing.Color.Sienna;
             this.btnAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAbrir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnAbrir.Location = new System.Drawing.Point(111, 270);
-            this.btnAbrir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAbrir.Location = new System.Drawing.Point(74, 176);
             this.btnAbrir.Name = "btnAbrir";
-            this.btnAbrir.Size = new System.Drawing.Size(274, 92);
+            this.btnAbrir.Size = new System.Drawing.Size(183, 60);
             this.btnAbrir.TabIndex = 0;
             this.btnAbrir.Text = "Abrir Botonera";
             this.btnAbrir.UseVisualStyleBackColor = false;
@@ -51,22 +51,23 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(57, 44);
+            this.label1.Location = new System.Drawing.Point(38, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(352, 69);
+            this.label1.Size = new System.Drawing.Size(239, 46);
             this.label1.TabIndex = 1;
             this.label1.Text = "Bienvenido!";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frmControles
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.ForestGreen;
-            this.ClientSize = new System.Drawing.Size(478, 444);
+            this.ClientSize = new System.Drawing.Size(319, 289);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAbrir);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmControles";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Inicio";
