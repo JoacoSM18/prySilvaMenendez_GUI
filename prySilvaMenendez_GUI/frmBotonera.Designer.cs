dc1de53bafcbe432caac64dcbdc05bb12e852b3b
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-17 12:06:17 -0300

    Tab Index cambiado

--- prySilvaMenendez_GUI/frmBotonera.Designer.cs
+++ prySilvaMenendez_GUI/frmBotonera.Designer.cs
@@ -38,11 +38,10 @@
             // 
             this.btnSiguiente.BackColor = System.Drawing.Color.Linen;
             this.btnSiguiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSiguiente.Location = new System.Drawing.Point(437, 254);
-            this.btnSiguiente.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSiguiente.Location = new System.Drawing.Point(291, 165);
             this.btnSiguiente.Name = "btnSiguiente";
-            this.btnSiguiente.Size = new System.Drawing.Size(129, 78);
-            this.btnSiguiente.TabIndex = 15;
+            this.btnSiguiente.Size = new System.Drawing.Size(86, 51);
+            this.btnSiguiente.TabIndex = 1;
             this.btnSiguiente.Text = "----->";
             this.btnSiguiente.UseVisualStyleBackColor = false;
             this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
@@ -50,11 +49,10 @@
             // lblDatos
             // 
             this.lblDatos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDatos.Location = new System.Drawing.Point(240, 109);
-            this.lblDatos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDatos.Location = new System.Drawing.Point(160, 71);
             this.lblDatos.Name = "lblDatos";
-            this.lblDatos.Size = new System.Drawing.Size(188, 53);
-            this.lblDatos.TabIndex = 9;
+            this.lblDatos.Size = new System.Drawing.Size(125, 34);
+            this.lblDatos.TabIndex = 0;
             this.lblDatos.Text = "Nombres";
             this.lblDatos.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -65,11 +63,10 @@
             "Luca",
             "Josefina",
             "Pablo"});
-            this.comboBox1.Location = new System.Drawing.Point(248, 460);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Location = new System.Drawing.Point(165, 299);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(201, 28);
-            this.comboBox1.TabIndex = 16;
+            this.comboBox1.Size = new System.Drawing.Size(135, 21);
+            this.comboBox1.TabIndex = 3;
             this.comboBox1.Text = "Seleccione Un Nombre";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -77,26 +74,24 @@
             // 
             this.btnAtras.BackColor = System.Drawing.Color.Linen;
             this.btnAtras.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAtras.Location = new System.Drawing.Point(114, 254);
-            this.btnAtras.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAtras.Location = new System.Drawing.Point(76, 165);
             this.btnAtras.Name = "btnAtras";
-            this.btnAtras.Size = new System.Drawing.Size(129, 78);
-            this.btnAtras.TabIndex = 17;
+            this.btnAtras.Size = new System.Drawing.Size(86, 51);
+            this.btnAtras.TabIndex = 2;
             this.btnAtras.Text = "<-----";
             this.btnAtras.UseVisualStyleBackColor = false;
             this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
             // 
             // frmBotonera
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.RosyBrown;
-            this.ClientSize = new System.Drawing.Size(670, 586);
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(447, 381);
             this.Controls.Add(this.btnAtras);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btnSiguiente);
             this.Controls.Add(this.lblDatos);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmBotonera";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Controles de Interfaz Grafica";
